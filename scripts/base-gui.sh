c30c3a543f41c14b151fb1f0649faa14eab0a829
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-01-07 16:55:25 +0000

    Attempt to add a proxy to yum puppet install

--- scripts/base-gui.sh
+++ scripts/base-gui.sh
@@ -17,7 +17,7 @@ echo "Installing base packages"
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl grub2-tools net-tools
 yum -y install epel-release.noarch
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm --httpproxy proxy.man.ac.uk --httpport 3128
 yum -y install puppet
 gem install hiera-eyaml hiera-eyaml-kms
 
