6492cfe9d3966ef0e96eb346e20d5152b064941b
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-01-22 13:57:48 +0000

    Added templates-c8 directory v3

--- templates-c8/templates-master.sh
+++ templates-c8/templates-master.sh
@@ -5,8 +5,8 @@ TMPDIR=/data/tmp/packer
 export http_proxy="http://proxy.man.ac.uk:3128"
 http_proxy="http://proxy.man.ac.uk:3128"
 
-#MASTERURL="http://mirrors.ukfast.co.uk/sites/ftp.centos.org/7/isos/x86_64"
-MASTERURL="http://ftp.pbone.net/pub/centos/7/isos/x86_64"
+#MASTERURL="http://mirrors.ukfast.co.uk/sites/ftp.centos.org/8/isos/x86_64"
+MASTERURL="http://ftp.pbone.net/pub/centos/8/isos/x86_64"
 
 echo -e "\nPlease select which image to create...\n"
 echo -e "\n1) Juan with GUI (Code Aster & Code Saturn)"
@@ -95,20 +95,20 @@ do
 	fi
 done
 cat ./templates/other/template-middle.json >> $OUTFILE
-echo -e "          \"output\": \"centos7-$BOX_NAME.box\"" >> $OUTFILE
+echo -e "          \"output\": \"centos8-$BOX_NAME.box\"" >> $OUTFILE
 cat ./templates/other/template-nearbottom.json >> $OUTFILE
 
 export http_proxy="http://proxy.man.ac.uk:3128"
-#wget $MASTERURL/sha256sum.txt -P /tmp
+wget $MASTERURL/sha256sum.txt -P /tmp
 
-#CHECKSUM=`grep Minimal /tmp/sha256sum.txt | grep iso | cut -d' ' -f1`
-#ISOURL=`grep Minimal /tmp/sha256sum.txt | grep iso | cut -d' ' -f3`
+CHECKSUM=`grep Minimal /tmp/sha256sum.txt | grep iso | cut -d' ' -f1`
+ISOURL=`grep Minimal /tmp/sha256sum.txt | grep iso | cut -d' ' -f3`
 
-#echo -e "      \"iso_checksum\": \"$CHECKSUM\"," >> $OUTFILE
-#echo -e "      \"iso_url\": \"$MASTERURL/$ISOURL\"," >> $OUTFILE
+echo -e "      \"iso_checksum\": \"$CHECKSUM\"," >> $OUTFILE
+echo -e "      \"iso_url\": \"$MASTERURL/$ISOURL\"," >> $OUTFILE
 
-echo -e "      \"iso_checksum\": \"9a2c47d97b9975452f7d582264e9fc16d108ed8252ac6816239a3b58cef5c53d\"," >> $OUTFILE
-echo -e "      \"iso_url\": \"file:///data/isos/CentOS-7-x86_64-Minimal-1908.iso\"," >> $OUTFILE
+#echo -e "      \"iso_checksum\": \"9a2c47d97b9975452f7d582264e9fc16d108ed8252ac6816239a3b58cef5c53d\"," >> $OUTFILE
+#echo -e "      \"iso_url\": \"file:///data/isos/CentOS-8-x86_64-Minimal-1908.iso\"," >> $OUTFILE
 
 cat ./templates/other/template-bottom.json >> $OUTFILE
 
@@ -116,10 +116,10 @@ TMPDIR=/data/tmp/packer packer build $OUTFILE
 
 rm -rf /data/tmp/post-packer/$BOX_NAME
 mkdir /data/tmp/post-packer/$BOX_NAME
-mv centos7-$BOX_NAME.box /data/tmp/post-packer/$BOX_NAME/.
+mv centos8-$BOX_NAME.box /data/tmp/post-packer/$BOX_NAME/.
 cd /data/tmp/post-packer/$BOX_NAME
-tar -xvzf centos7-$BOX_NAME.box
+tar -xvzf centos8-$BOX_NAME.box
 sed -i "s/end/  config.ssh.password = 'vagrant'\nend/g" Vagrantfile
-tar -cvzf centos7-$BOX_NAME-v2.box Vagrantfile metadata.json box.ovf packer-virtualbox-iso-*.vmdk
+tar -cvzf centos8-$BOX_NAME-v2.box Vagrantfile metadata.json box.ovf packer-virtualbox-iso-*.vmdk
 
-mv centos7-$BOX_NAME-v2.box /data/saved_images/templates/centos7-$BOX_NAME.box
+mv centos8-$BOX_NAME-v2.box /data/saved_images/templates/centos8-$BOX_NAME.box
