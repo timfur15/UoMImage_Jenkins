cc8af545bb92f5ddb65c69fb5a4b492665ac994e
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-02-04 10:04:33 +0000

    Added new terraform script file v2

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-date > /etc/vagrant_box_build_time
+systemctl stop firewalld
+systemctl disable firewalld
 
-mkdir -pm 700 /home/vagrant/.ssh
-curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys -x http://proxy.man.ac.uk:3128
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+sed -i '/.*proxy.*/d' /etc/yum.conf
+
+sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
+systemctl restart sshd
+
+mkdir /root/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2JDsYtz5MuBX7LCb16rn4TNswhCIfJeqMEVoW+Vk9R0EZOX3RLXqhKc3zWApPnMvV5iMixPhSOeXvgYsuS0fwJyCemvn2pdB3tl/1tLfcJj39aEIGtmJ6pNxFswRDlvYAZthBXS5NGkQ3KyYee7sXm2lRB1QfLEiO/4lTQQsV45nDQses5n9I7phIoVWzf9/xeKTQO9gIlkr+LQ5SSVFnv43wkZSMsC24p1oTZ8L7bkHkNdq96GqIbsCxQQr+PNMzePHC2I5V9/BPQdBL54LjX670qdcamrxhTsnYfXtOC0tmPDRBUxuFFIt4yOzS/uJTgeNAxywgsozMxxR1d4Hl vagrant@ip-172-31-90-93" > /root/.ssh/authorized_keys
