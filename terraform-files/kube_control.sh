bb63fecaa20cf46dbe16fdcbcf94cb919bfbed08
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-02-04 15:30:50 +0000

    Added kube_control.sh script to terraform files directory v9

--- terraform-files/kube_control.sh
+++ terraform-files/kube_control.sh
@@ -3,14 +3,13 @@
 export HTTPS_PROXY=http://proxy.man.ac.uk:3128
 HTTPS_PROXY=http://proxy.man.ac.uk:3128
 
-wget https://github.com/kubermatic/kubeone/releases/download/v0.9.2/kubeone_0.9.2_linux_amd64.zip
-https://github.com/kubermatic/kubeone/releases/download/v0.10.0/kubeone_0.10.0_linux_amd64.zip
+wget https://github.com/kubermatic/kubeone/releases/download/v0.10.0/kubeone_0.10.0_linux_amd64.zip
 unzip kubeone_0.10.0_linux_amd64.zip 
 mv kubeone /usr/local/bin/.
 kubeone completion bash > /etc/bash_completion.d/kubeone
 
-wget https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
-unzip terraform_0.12.9_linux_amd64.zip 
+wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
+unzip terraform_0.12.20_linux_amd64.zip 
 mv terraform /usr/local/bin/.
 
 eval `ssh-agent`
