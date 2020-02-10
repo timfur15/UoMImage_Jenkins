c514e33e966dfed19929437420b406176f9eb492
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-02-10 15:59:12 +0000

    Added Docker Swarm Node v2

--- scripts/swarm.sh
+++ scripts/swarm.sh
@@ -5,7 +5,26 @@ echo "proxy=http://proxy.man.ac.uk:3128" >> /etc/yum.conf
 exec >> /tmp/docker-swarm.log
 exec 2>&1
 
-yum -y install docker iptables-services
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine -y
+
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io -y
+
+yum -y install iptables-services
 
 systemctl stop firewalld
 systemctl disable firewalld
@@ -13,9 +32,9 @@ systemctl disable firewalld
 systemctl enable docker
 systemctl start docker
 
+yum -y install iptables-services
 systemctl enable iptables
 wget https://raw.githubusercontent.com/timfur15/UoMImage_Jenkins/master/terraform-files/swarm-iptables
 mv iptables /etc/sysconfig/.
 systemctl start iptables
-
 
