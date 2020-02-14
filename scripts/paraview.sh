250fae4669c7f4986ba606a15f3405229ca77495
root <root@vm-rvmi-its038.its.manchester.ac.uk>
2020-02-14 13:47:30 +0000

    Changed Juan to add paraview and salome_meca v3

--- scripts/paraview.sh
+++ scripts/paraview.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+export https_proxy=http://proxy.man.ac.uk:3128
+https_proxy=http://proxy.man.ac.uk:3128
+
 cd /root
 wget -O paraview.tgz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.6&type=binary&os=Linux&downloadFile=ParaView-5.6.2-MPI-Linux-64bit.tar.gz"
 tar -xvzf paraview.tgz
 
 #/root/ParaView-5.6.2-MPI-Linux-64bit/bin/pvserver --force-offscreen-rendering
+
+unset https_proxy
